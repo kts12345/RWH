a56f3f3cb1571c050cb1609f79d11438dde28daa
kts123 <taeksoon@gmail.com>
2015-03-08 06:20:43 -0700

    RHS

---
+++ src/Main.hs
@@ -0,0 +1,10 @@
+-- | Main entry point to the application.
+module Main where
+
+import Ch01Exer01
+
+-- | The main entry point.
+main :: IO ()
+main = do
+    putStrLn "Welcome to FP Haskell Center!"
+    putStrLn "Have a good day!"
