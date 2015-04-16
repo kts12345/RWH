ca4279b19f7472348418cc903b6e08f1495365b4
kts123 <taeksoon@gmail.com>
2015-04-16 10:06:16 -0700

    Ch04Exer01_03

--- src/Ch04Exer01_03.hs
+++ src/Ch04Exer01_03.hs
@@ -1,1 +1,28 @@
 module Ch04Exer01_03 where
+
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main' = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction xs = unlines      $
+                        map safeHead $
+                        map words    $
+                        lines xs
+              where safeHead [] = []
+                    safeHead xs = head xs
+
+--------------------------------------------------------------
+-- | The solution's main entry point
+solution :: IO ()
+solution = do
+    main'
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 -- | Main entry point to the application.
 module Main where
 
-import Ch04Exer01_02
+import Ch04Exer01_03
 
 -- | The main entry point.
 main :: IO ()
